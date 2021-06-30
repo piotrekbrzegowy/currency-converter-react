34d6f89fdbfeaa39e06d4d4e1fb93c50e359309a
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-06-30 12:26:56 +0200

    currency converter with interactions

---
+++ src/App/Form/Result/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+export const Result = ({result}) => (
+    <p className="result">
+        {result !== undefined && (
+            <>
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
+                {" "}
+                <strong>
+                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                </strong>
+            </>
+        )}
+    </p>
+);
+
---
+++ src/App/index.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Form } from './Form';
+import { currencies } from './currencies';
+import { useState } from 'react';
+
+function App() {
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+      .find(({ short }) => short === currency)
+      .rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
+
+  return (
+    <div className="app">
+      <Form
+        result={result}
+        calculateResult={calculateResult}
+      />
+    </div>
+  );
+}
+
+export default App;
